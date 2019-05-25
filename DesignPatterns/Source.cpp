4bbfe07c5502b4e29d143b33c379db05270b3598
MajaDev1983 <majastipetic22@gmail.com>
2019-05-25 15:23:01 +0200

    singleton

--- DesignPatterns/Source.cpp
+++ DesignPatterns/Source.cpp
@@ -1,23 +1,12 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "window.h"
+
+//https://www.sfml-dev.org/tutorials/2.5/start-vc.php --> ako nekaj zasteka, pomoc
+//https://sourcemaking.com/design_patterns
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-
-
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-
-		window.display();
-	}
-
+	//std::cout << window::getInstance()->getWindowName() << std::endl;
+	Window::getInstance()->update();
 	return 0;
 }
---
+++ DesignPatterns/window.cpp
@@ -0,0 +1,46 @@
+#include "window.h"
+Window* Window::instance = 0;
+
+Window* Window::getInstance()
+{
+	if (instance == nullptr)
+	{
+		instance = new Window();
+		return instance;
+	}
+	else
+	{
+		return instance;
+	}
+}
+
+std::string Window::getWindowName()
+{
+	return "My singleton window";
+}
+
+void Window::update()
+{
+	while (this->renderContext->isOpen())
+	{
+		sf::Event event;
+		while (this->renderContext->pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				this->renderContext->close();
+		}
+
+		this->renderContext->clear();
+		this->renderContext->display();
+	}
+}
+
+Window::Window()
+{
+	this->renderContext = new sf::RenderWindow(sf::VideoMode(800, 600), this->getWindowName());
+}
+
+
+Window::~Window()
+{
+}
